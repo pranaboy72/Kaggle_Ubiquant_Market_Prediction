3a6f410d48bd35105d5203688a09461539d500e1
pranaboy72 <86711384+pranaboy72@users.noreply.github.com>
2022-02-10 20:59:54 +0900

    Update predict.py

--- markt_prdct/predict.py
+++ markt_prdct/predict.py
@@ -5,6 +5,7 @@ from sklearn.linear_model import LinearRegression
 
 # visulize 할 index 정하기
 idx=4
+idx_2=6
 
 # csv 파일을 리스트로 변환해주기
 ## train data
@@ -13,10 +14,13 @@ f=open("/content/train.csv","r") # 디렉토리는 알아서 설정
 reader=csv.reader(f)
 count=0
 train_data=[]
+train_data_2=[]
 
 for row in reader:
   if count == idx :
     train_data.append(row)
+  elif count == idx_2:
+    train_data_2.append(row)
     break
   else:
     count+=1
@@ -25,10 +29,16 @@ f.close()
 # Linear Regression
 plt_dt=train_data[0][4:]
 plt_data=np.array(plt_dt).reshape((1,-1))
-t=np.array(range(len(plt_data))).reshape((1,-1))
+plt_dt_2=train_data_2[0][4:]
+plt_data_2=np.array(plt_dt_2).reshape((1,-1))
+
+target=(np.array(train_data[0][3])).reshape((-1,1))
+target_2=(np.array(train_data_2[0][3])).reshape((-1,1))
+
+t=range(len(plt_dt))
 
 reg=LinearRegression()
-reg.fit(t,plt_data)
+reg.fit(,plt_data)
 coef=reg.coef_
 intercept=reg.intercept_
 
