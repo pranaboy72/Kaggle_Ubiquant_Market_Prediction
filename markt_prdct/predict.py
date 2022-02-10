f28cdfef40c36b652b15fee5ed7ab44278534fd7
pranaboy72 <86711384+pranaboy72@users.noreply.github.com>
2022-02-10 23:30:40 +0900

    Update predict.py

--- markt_prdct/predict.py
+++ markt_prdct/predict.py
@@ -32,23 +32,27 @@ plt_data=np.array(plt_dt).reshape((1,-1))
 plt_dt_2=train_data_2[0][4:]
 plt_data_2=np.array(plt_dt_2).reshape((1,-1))
 
-target=(np.array(train_data[0][3])).reshape((-1,1))
+data=[plt_data, plt_data_2]
+
+target_1=(np.array(train_data[0][3])).reshape((-1,1))
 target_2=(np.array(train_data_2[0][3])).reshape((-1,1))
+
+target=[target_1, target_2]
 
 t=range(len(plt_dt))
 
 reg=LinearRegression()
-reg.fit(,plt_data)
+reg.fit(data,target)
 coef=reg.coef_
 intercept=reg.intercept_
 
 ## Build the linear function
-def f(x):
-  return float(coef)*x + float(intercept)
+def f(x,co,inter):
+  return sum(list(co)*list(x)) + inter
 
 #plot
 plt.plot(t,plt_dt,'b')
-plt.plot(t,f(t),'r')
+plt.plot(t,f(t,coef,float(intercept)),'r')
 plt.xlabel('Time Step')
 plt.ylabel('Price')
 plt.show()
