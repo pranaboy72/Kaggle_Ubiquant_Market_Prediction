c047b3030713b63dc195b17b91086475879ba9c5
pranaboy72 <86711384+pranaboy72@users.noreply.github.com>
2022-02-15 01:28:39 +0900

    Update linear_regression.py

--- markt_prdct/linear_regression.py
+++ markt_prdct/linear_regression.py
@@ -4,8 +4,7 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 
 # visulize 할 index 정하기
-idx=4
-idx_2=6
+idx=100 # 무조건 10의 배수!!!!!!!!!!!!
 
 # csv 파일을 리스트로 변환해주기
 ## train data
@@ -16,44 +15,65 @@ count=0
 train_data=[]
 train_data_2=[]
 
-for row in reader:
-  if count == idx :
-    train_data.append(row)
-  elif count == idx_2:
-    train_data_2.append(row)
+# csv파일의 데이터 값들을 반복문으로 이중리스트 만들어주기
+f = open("/content/train.csv",'r')
+count=0
+train_data=[]
+test_data=[]
+
+while True:
+  line=f.readline() # 먼저, 오픈한 f를 한줄한줄 읽어온다
+  
+  if 0 < count < idx*0.7+1:  # 각 열에 대한 설명인 첫행은 스킵하기 위해 0 초과, 원하는 데이터 개수의 70퍼센트만큼 train data로
+    append_data=line.split(",") # 불러온 행의 요소들을 ',' 단위로 스플릿해준다. (csv 파일이기 때문)
+    append_data=list(map(float,append_data)) # 스플릿해준 각 요소들이 스트링 값이라 이를 float로 변환
+    
+    train_data.append(append_data) # 만들어낸 append data를 train_data에 다시 넣어주면 이중리스트 형성(매 루프마다 리스트가 한개씩 들어가는 꼴)
+                                   # 이렇게 만들어낸 train data로 선형회귀 모델을 만들 것이다
+      
+  if idx*0.7 < count < idx+1: # 만든 모델을 평가할 test data를 만든다, 전체 데이터의 30퍼센트만큼
+    append_data=line.split(",")
+    append_data=list(map(float,append_data))
+
+    test_data.append(append_data)
+  
+  if count==idx+1:  # 원하는 만큼 뽑아냈으면 break
     break
-  else:
-    count+=1
+
+  count+=1  # 행 세주기
 f.close()
+count-=1 # 각 열들을 나타내주는 0행을 제외하고 넣어줬기 때문에 다시 빼줘야 얻어낸 총 데이터 개수이다
 
+# train data 의 target 값들 모아둔 리스트 만들기
+f_data=[]
+target=[]
+
+for i in range(int(count*0.7)):  # 아까 구한 count 를 활용하여 뽑아낸 행의 개수를 구한다
+  append_target=[]
+  append_target.append(train_data[i][3])  # target data는 더 복잡하게 구하는 이유는 그냥 append 해버리면 이중리스트가 되지 않는다
+  target.append(append_target)
+  f_data.append(train_data[i][4:])
+
+# 똑같이 test data 다듬기
+test_fdata=[]
+test_target=[]
+
+for i in range(int(count*0.3)):
+  test_fdata.append(test_data[i][4:])
+  test_target.append(test_data[i][3])  # test data의 target은 선형회귀를 하는게 아니라 나중에 평가용이므로 이중리스트로 만들지 않는다
+  
 # Linear Regression
-plt_dt=train_data[0][4:]
-plt_data=np.array(plt_dt).reshape((1,-1))
-plt_dt_2=train_data_2[0][4:]
-plt_data_2=np.array(plt_dt_2).reshape((1,-1))
-
-data=np.concatenate([plt_data, plt_data_2],axis=0)
-
-target_1=(np.array(train_data[0][3])).reshape((-1,1))
-target_2=(np.array(train_data_2[0][3])).reshape((-1,1))
-
-target=np.concatenate([target_1, target_2],axis=0)
-
-t=range(len(plt_dt))
-
 reg=LinearRegression()
-reg.fit(data,target)
-coef=(reg.coef_).reshape(-1)
-intercept=reg.intercept_
+reg.fit(f_data,target)
 
-## Build the linear function
-def f(x,co,inter):
-  return co*x + inter
+# Predict & Plot
+predict=reg.predict(test_fdata)
+predict=np.array(predict).reshape(-1)
 
-#plot
-plt.plot(t,plt_dt,'b')
-plt.plot(t,plt_dt_2,'c')
-plt.plot(t,f(t,coef,float(intercept)),'r')
-plt.xlabel('Time Step')
+t=list(range(int(count*0.3)))
+plt.plot(t,test_target,'r',label='target')
+plt.plot(t,predict,'b',label='predicted')
+plt.xlabel('Data')
 plt.ylabel('Price')
+plt.legend(loc='best', ncol=2)
 plt.show()
